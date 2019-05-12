6f10338c0be09945c5b353d764c35ee6f3b98d9e
iamvaibhavkr <vaibhav.169105211@muj.manipal.edu>
2019-05-13 01:14:23 +0530

    Create Mia.py

---
+++ Mia.py
@@ -0,0 +1,146 @@
+import pyttsx3
+import webbrowser
+import smtplib
+import random
+import speech_recognition as sr
+import wikipedia
+import datetime
+import wolframalpha
+import os
+import sys
+
+engine = pyttsx3.init('sapi5')
+
+client = wolframalpha.Client('XWTHV9-R9YUWU9HW3')
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[len(voices)-1].id)
+
+def speak(audio):
+    print('Mia : ' + audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+def greetMe():
+    now = datetime.datetime.now()
+    td=now.strftime("********* %d-%m-%Y  %H:%M:%S *********")
+    print(td)
+    currentH = int(datetime.datetime.now().hour)
+    if currentH >= 0 and currentH < 12:
+        speak('Good Morning!')
+
+    if currentH >= 12 and currentH < 18:
+        speak('Good Afternoon!')
+
+    if currentH >= 18 and currentH !=0:
+        speak('Good Evening!')
+
+greetMe()
+
+speak('Hello Sir! I am your digital assistant, Mia!') 
+speak('How may I help you?')
+
+
+def myCommand():
+   
+    r = sr.Recognizer()                                                                                   
+    with sr.Microphone() as source:                                                                       
+        print("Listening...")
+        r.pause_threshold =  1
+        audio = r.listen(source)
+    try:
+        query = r.recognize_google(audio, language='en-in')
+        print('Admin : ' + query + '\n')
+        
+    except sr.UnknownValueError:
+        speak('Sorry sir! I didn\'t get that! Try typing the command!')
+        query = str(input('Command : '))
+
+    return query
+        
+
+if __name__ == '__main__':
+
+    while True:
+    
+        query = myCommand();
+        query = query.lower()
+        
+        if 'open youtube' in query:
+            speak('okay')
+            webbrowser.open('www.youtube.com')
+
+        elif 'open google' in query:
+            speak('okay')
+            webbrowser.open('www.google.co.in')
+
+        elif 'open gmail' in query:
+            speak('okay')
+            webbrowser.open('www.gmail.com')
+
+        elif "what\'s up" in query or 'how are you' in query:
+            stMsgs = ['Just doing my thing!','Nothing much really, What\'s up with you?', 'Cool!, You?' 'I am fine!', 'Nice!', 'I am good and full of energy']
+            speak(random.choice(stMsgs))
+
+        elif 'email' in query:
+            speak('Who is the recipient? ')
+            recipient = myCommand()
+
+            if 'me' in recipient:
+                try:
+                    speak('What should I say? ')
+                    content = myCommand()
+        
+                    server = smtplib.SMTP('smtp.gmail.com', 587)
+                    server.ehlo()
+                    server.starttls()
+                    server.login("Your_Username", 'Your_Password')
+                    server.sendmail('Your_Username', "Recipient_Username", content)
+                    server.close()
+                    speak('Email sent!')
+
+                except:
+                    speak('Sorry Sir! I am unable to send your message at this moment!')
+
+
+        elif 'nothing' in query or 'abort' in query or 'stop' in query or 'see you' in query or 'bye' in query:
+            speak('okay')
+            speak('Bye Sir, have a wonderful day.')
+            sys.exit()
+
+        elif 'thank you' in query or 'thanks' in query or 'wow' in query or 'great' in query:
+            speak('You\'re Welcome sir!')
+           
+        elif 'hello' in query:
+            speak('Hello Sir')
+                                    
+        elif 'play music' in query:
+            music_folder = Your_music_folder_path
+            music = [music1, music2, music3, music4, music5]
+            random_music = music_folder + random.choice(music) + '.mp3'
+            os.system(random_music)
+                  
+            speak('Okay, here is your music! Enjoy!')
+            
+
+        else:
+            query = query
+            speak('Searching...')
+            try:
+                try:
+                    res = client.query(query)
+                    results = next(res.results).text
+                    speak('Got it.')
+                    speak('According to WOLFRAM-ALPHA - ')
+                    speak(results)
+                    
+                except:
+                    results = wikipedia.summary(query, sentences=2)
+                    speak('Got it.')
+                    speak('WIKIPEDIA says - ')
+                    speak(results)
+        
+            except:
+                webbrowser.open('www.google.com')
+        
+        speak('Anything else, Sir?')
